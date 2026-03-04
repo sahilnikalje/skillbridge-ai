e64706304824d4437f69bd8af225837abd237f7d
sahilnikalje <sahilnik88@gmail.com>
2026-03-04 17:13:06 +0530

    db added

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const app=require('./src/app')
+const connectDB = require('./src/config/db')
+
+
+
+const PORT=process.env.PORT
+
+
+const startServer=async()=>{
+    try{
+        await connectDB()
+        app.listen(PORT, ()=>{
+            console.log(`Server running on port ${PORT}`)
+        })
+    }
+    catch(err){
+        console.log(err.message)
+        process.exit(1)
+    }
+}
+startServer()
---
+++ Backend/src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose')
+
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to DB")
+    }
+    catch(err){
+        console.log("connectDBErr: ", err.message)
+        process.exit(1)
+    }
+}
+module.exports=connectDB
