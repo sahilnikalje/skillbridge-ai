80edeacb67da7d5d0fe3a0515e9012f11bfa4a40
sahilnikalje <sahilnik88@gmail.com>
2026-03-07 00:25:39 +0530

    reports page added, logout button added

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -4,6 +4,7 @@ import Register from './features/auth/pages/Register'
 import Protected from './features/auth/components/Protected'
 import Home from './features/interview/pages/Home'
 import Interview from './features/interview/pages/Interview'
+import Reports from './features/interview/pages/Reports'
 
 
 export const router=createBrowserRouter([
@@ -18,6 +19,10 @@ export const router=createBrowserRouter([
     {
         path:'/',
         element:<Protected><Home/></Protected>
+    },   
+     {
+        path:'/reports',
+        element:<Protected><Reports/></Protected>
     },
     {
         path:'/interview/:interviewId',
