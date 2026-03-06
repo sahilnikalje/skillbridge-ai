b0bcf037870f56f86998d9183d047c0b656bc0ad
sahilnikalje <sahilnik88@gmail.com>
2026-03-06 22:52:57 +0530

    interview.api.js changed

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -2,8 +2,15 @@ import axios from "axios";
 
 const API=axios.create({
     baseURL:import.meta.env.VITE_API_URL,
-    withCredentials:true
+    // withCredentials:true
 })
+
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token')
+    if (token) config.headers.Authorization = `Bearer ${token}`
+    return config
+})
+
 
 export const generateInterviewReport=async({jobDescription, selfDescription, resumeFile})=>{
     const formData=new FormData()
